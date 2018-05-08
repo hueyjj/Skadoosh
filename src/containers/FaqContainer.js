508e59ecbde86c337f1d7a55c62dfeb618cec017
Trevor Jordy <trevorljordy@gmail.com>
2018-05-08 14:59:53 -0700

    added faq container file

---
+++ src/containers/FaqContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import Faq from '../components/Faq';
+
+const FaqContainer = props => <Faq {...props} />;
+
+const mapStateToProps = (state) => {
+  return {
+  }
+};
+
+const mapDispatchToProps = {
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(FaqContainer);
